7afc5756bcbdeb44bb02378406412174a8e0f47d
Teddy <dudetotheleft@gmail.com>
2023-07-11 15:14:03 -0700

    Implemented Buzzfeed method, wrapped menu code into its own method, minor text/aesthetic adjustments

--- Program.cs
+++ Program.cs
@@ -1,38 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, GitHub!");
+﻿Console.WriteLine("Hello, GitHub users!");
+MainMenu();
 
-Console.WriteLine("What would you like to browse? \n\nA. Buzzfeed Quiz \nB. ASCII Cats \nC. Homemade Cookie Recipes \n");
-string choice = Console.ReadLine().ToUpper();
-switch (choice)
+static void MainMenu()
 {
-    case "A":
-        Console.WriteLine();
-        Buzzfeed();
-        break;
-    
-    case "B":
-        Console.WriteLine();
-        Cats();
-        break;
+    Console.WriteLine("What would you like to browse?");
+    Console.WriteLine("\nA. ASCII Cats \nB. Buzzfeed Quiz \nC. Homemade Cookie Recipe \n");
+    string choice = Console.ReadLine().ToUpper();
 
-    case "C":
-        Console.WriteLine();
-        Cookies();
-        break;
+    switch (choice)
+    {
+        case "A":
+            Console.WriteLine();
+            Cats();
+            break;
 
-    default:
-        Console.WriteLine("Something went wrong! Try again :(");
-        break;
+        case "B":
+            Console.WriteLine();
+            Buzzfeed();
+            break;
+
+        case "C":
+            Console.WriteLine();
+            Cookies();
+            break;
+
+        default:
+            Console.WriteLine("Something went wrong! Try again :(");
+            break;
+    }
 }
 
-static void Buzzfeed()
-{
-    throw new NotImplementedException();
-}
 
 static void Cats()
 {
     throw new NotImplementedException();
+}
+
+static void Buzzfeed()
+{
+
+    Console.WriteLine("( Enter 'Z' to quit )");
+    Console.WriteLine("BUZZFEED! This simple question will decide what kind of cake you are. Pick one:");
+    Console.WriteLine("A. Cat \nB. Dog \nC. Mouse \nD. Horse");
+
+    string choice = Console.ReadLine().ToUpper();
+    switch (choice)
+    {
+        case "A":
+            Console.WriteLine("Vanilla cake. Seemingly bland, but balanced.");
+            break;
+
+        case "B":
+            Console.WriteLine("Chocolate cake. You can't ever get enough.");
+            break;
+
+        case "C":
+            Console.WriteLine("Carrot cake. What, how'd you know already?");
+            break;
+
+        case "D":
+            Console.WriteLine("Strawberry shortcake. I don't know why, I don't make the rules.");
+            break;
+
+        case "Z":
+            break;
+
+        default:
+            Console.WriteLine("Sorry, something went wrong over at Buzzfeed HQ. Try entering A, B, C, or D next time.");
+            break;
+    }
+
+    Console.WriteLine("( Quiz over! ) \n\n");
+    MainMenu();
 }
 
 static void Cookies()
